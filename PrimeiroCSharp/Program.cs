3f090536d7c9762b47515a532e801b2d1a9b4d6e
Gerson Santos <gersones@outlook.com>
2021-03-19 19:25:41 -0300

    Criacao do projeto

---
+++ PrimeiroCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
