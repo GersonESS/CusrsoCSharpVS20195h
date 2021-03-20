b41064ab8953e2f26411bb8c74568eb58a5c2db2
Gerson Santos <gersones@outlook.com>
2021-03-19 23:58:09 -0300

    segunda alteracao

--- PrimeiroCSharp/Program.cs
+++ PrimeiroCSharp/Program.cs
@@ -10,16 +10,46 @@ namespace PrimeiroCSharp
             //float -152.20 232.32 decimal double
             // bool// string "Texto"
             // char 'a'
+            /*
             int segundaguerra = 1939;
-            float velocidade = 251.48f;
-            bool segundaguerramundial = true;
-            string cor = "Vermenho";
-            Console.WriteLine(segundaguerra);
-            Console.WriteLine(velocidade);
-            Console.WriteLine(segundaguerramundial);
+               float velocidade = 251.48f;
+               bool segundaguerramundial = true;
+               string cor = "Vermenho";
+               Console.WriteLine(segundaguerra);
+               Console.WriteLine(velocidade);
+               Console.WriteLine(segundaguerramundial);
+               Console.WriteLine(cor);
+
+
+               var velocidade = 251.48f;
+               var cor = "Vermenho";
+              
+             
+            dynamic cor = "Vermenho";
             Console.WriteLine(cor);
+            cor = 251;
+            Console.WriteLine(cor);
+            
+            const float PI = 3.14f;
+            
 
             Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seu NOME é");
+            Console.WriteLine(nome);
+            */
+            int num = 20 + 20 - 50;
+            Console.WriteLine(num);
+            int mult = 20 * 20 * 2;
+            Console.WriteLine(mult);
+
+
+
+
+
+
 
         }
     }
