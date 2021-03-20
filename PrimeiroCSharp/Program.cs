c8a010ef49c0fd39c57ca1bdabd726d194d83d27
Gerson Santos <gersones@outlook.com>
2021-03-19 22:05:59 -0300

    preparacao inicial

--- PrimeiroCSharp/Program.cs
+++ PrimeiroCSharp/Program.cs
@@ -6,7 +6,21 @@ namespace PrimeiroCSharp
     {
         static void Main(string[] args)
         {
+            //int -156 3233
+            //float -152.20 232.32 decimal double
+            // bool// string "Texto"
+            // char 'a'
+            int segundaguerra = 1939;
+            float velocidade = 251.48f;
+            bool segundaguerramundial = true;
+            string cor = "Vermenho";
+            Console.WriteLine(segundaguerra);
+            Console.WriteLine(velocidade);
+            Console.WriteLine(segundaguerramundial);
+            Console.WriteLine(cor);
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
